9d28b84a444511bb408d66aaadec9f19195141a7
Simon Wachter <simon@Simons-Laptop.local>
2023-09-16 15:58:07 +0200

    initial commit

--- pages/api/getMaps/index.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "../../../lib/prisma";
-
-// POST /api/getMaps
-export default async function handle(req, res) {
-  const result = await prisma.map.findMany({
-    include: { Set: true },
-  });
-  res.json(result);
-}
--- pages/api/getSets/index.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "../../../lib/prisma";
-
-// POST /api/getSets
-export default async function handle(req, res) {
-  const result = await prisma.set.findMany({
-    include: { points: true, winner: true, map: true },
-  });
-  res.json(result);
-}
